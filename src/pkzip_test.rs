9a12a4215fef56bc39e3102ff2f868cf107e8fc6
Nathan Button <nathanebutton@gmail.com>
2022-06-16 23:17:37 -0500

    Make bidirectional byte array test more thorough

--- src/pkzip_test.rs
+++ src/pkzip_test.rs
@@ -4,10 +4,14 @@ use crate::pkzip::ByteArray;
 // Test that bytes can be bidirectionally cast with bits
 fn byte_array_bidirectional_test()
 {
-    let byte_array = ByteArray::new(15u8);
-    println!("{:#?}", byte_array.bits);
-    println!("{:#?}", 15u8);
-    let byte = byte_array.get_byte();
-    println!("{:#?}", byte);
-    assert_eq!(byte, 15u8);
+    for i in 0u8..=255u8
+    {
+
+        let byte_array = ByteArray::new(i);
+        println!("{:#?}", byte_array.bits);
+        println!("{:#?}", i);
+        let byte = byte_array.get_byte();
+        println!("{:#?}", byte);
+        assert_eq!(byte, i);
+    }
 }
